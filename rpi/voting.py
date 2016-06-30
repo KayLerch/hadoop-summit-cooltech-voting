293b1cf67ad5bd722ff39107311a80bdb6112bf8
Kay Lerch <dekstar@gmx.de>
2016-06-30 21:33:22 +0200

    Fix a bug with the kilo dashes on the LED matrix counter

--- rpi/voting.py
+++ rpi/voting.py
@@ -123,9 +123,9 @@ def drawNumber(num):
     if len(numstr) == 4 : width = -9;
     graphics.DrawText(myMatrix, fontLarge, width, 21, white, numstr.encode("utf-8"))
 
-    fullThousand = num / 1000
+    fullThousand = round(num / 1000)
     if fullThousand >= 1:
-        while fullThousand > 0:
+        while fullThousand >= 1:
             graphics.DrawText(myMatrix, fontSmall, 1 + (3 * (fullThousand - 1)), 6, lightYellow, "|".encode("utf-8"))
             fullThousand = fullThousand - 1
 
